f4601dd54cfcb7d4325778d770f2a6f3c8c5e1b4
ThibBer <thibaut.berg@hotmail.com>
2025-04-13 19:39:07 +0200

    Update table wrap

--- src/main/kotlin/de/uni_passau/fim/se2/intelligame/components/LeaderboardPointsListDialog.kt
+++ src/main/kotlin/de/uni_passau/fim/se2/intelligame/components/LeaderboardPointsListDialog.kt
@@ -3,11 +3,15 @@
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.util.minimumHeight
+import com.intellij.util.ui.JBUI
 import de.uni_passau.fim.se2.intelligame.util.GameMode
 import de.uni_passau.fim.se2.intelligame.util.Util
 import java.awt.BorderLayout
+import java.awt.Component
 import javax.swing.*
 import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableCellRenderer
 import javax.swing.table.TableRowSorter
 
 class TablePopupDialog(project: Project?) : DialogWrapper(project) {
@@ -29,6 +33,49 @@ class TablePopupDialog(project: Project?) : DialogWrapper(project) {
         val table: JTable = object : JTable(tableModel) {
             override fun isCellEditable(row: Int, column: Int): Boolean {
                 return false
+            }
+        }
+
+        table.columnModel.getColumn(1).cellRenderer = object : JTextArea(), TableCellRenderer {
+            init {
+                lineWrap = true
+                wrapStyleWord = true
+                isOpaque = true
+                isEditable = false
+                margin = JBUI.insets(5)
+            }
+
+            override fun getRowHeight(): Int {
+                return table.rowHeight
+            }
+
+            override fun getTableCellRendererComponent(
+                jTable: JTable?,
+                value: Any?,
+                isSelected: Boolean,
+                hasFocus: Boolean,
+                row: Int,
+                column: Int
+            ): Component {
+                text = value.toString()
+                font = table.font
+                minimumHeight = table.rowHeight
+
+                if(jTable != null){
+                    setSize(jTable.columnModel.getColumn(column).width, getPreferredSize().height);
+
+                    if (jTable.getRowHeight(row) != getPreferredSize().height) {
+                        jTable.setRowHeight(row, getPreferredSize().height);
+                    }
+                }
+
+                return this;
+            }
+
+            override fun firePropertyChange(propertyName: String, oldValue: Boolean, newValue: Boolean) {
+                if ("lineWrap" == propertyName || "wrapStyleWord" == propertyName) {
+                    super.firePropertyChange(propertyName, oldValue, newValue)
+                }
             }
         }
 
